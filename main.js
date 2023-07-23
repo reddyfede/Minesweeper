922082ce2d6ab257db95f7070a854d053ff1f410
reddy <reddyfede@gmail.com>
2023-07-24 01:27:42 +0200

    add board reveal if empty tile is clicked

--- main.js
+++ main.js
@@ -25,14 +25,17 @@ for (btn of selectLevelBtnEls) {
 
 boardEl.addEventListener("click", clickTile)
 
+
 resetBtn.addEventListener("click", init)
 
 /*----- functions -----*/
 
 function init() {
+    boardEl.addEventListener("click", clickTile)
+
     bombCounter.innerText = ""
     bombsIdx = []
-    
+
 
     createBoard()
     grid = createGrid()
@@ -43,7 +46,7 @@ function init() {
     placeBombs()
     populateGrid()
 
-    // renderBoard()
+    renderBoard()
 }
 
 function defineBoard(e) {
@@ -79,7 +82,6 @@ function createBoard() {
         newBtn = document.createElement("button")
         newBtn.classList.add("gameTile")
         newBtn.setAttribute("id", `idx${i}`)
-        newBtn.innerText = ""
         boardEl.appendChild(newBtn)
     }
 }
@@ -96,24 +98,48 @@ function clickTile(e) {   //placeholder
     }
     e.target.disabled = true
     e.target.classList.add("clicked")
-    
 
-    let idx = [parseInt(e.target.getAttribute("id").slice(3))]
-    
-
+    let idx = parseInt(e.target.getAttribute("id").slice(3))
+    console.log(idx)
     if (grid[idx] === "B") {
-        for (el of grid){
-            if (el === "B"){
-                document.getElementById(`idx${el}`).innerText = grid[el]
+
+        e.target.classList.add("bomb")
+        for (el of bombsIdx) {
+            document.getElementById(`idx${el}`).innerText = grid[el]
+            document.getElementById(`idx${el}`).classList.add("clicked")
+            boardEl.removeEventListener("click", clickTile)
+        }
+
+    } else if (grid[idx] !== 0) {
+
+        e.target.innerText = grid[idx]
+
+    } else {
+
+        let checkIfReveal = checkGrid([idx])
+
+        console.log(checkIfReveal)
+
+        for (i = 0; i < checkIfReveal.length; i++) {
+
+            if (grid[checkIfReveal[i]] !== 0) {
+                console.log(grid[checkIfReveal[i]])
+                document.getElementById(`idx${checkIfReveal[i]}`).innerText = grid[checkIfReveal[i]]
+                document.getElementById(`idx${checkIfReveal[i]}`).classList.add("clicked")
+                document.getElementById(`idx${checkIfReveal[i]}`).disabled = true
+            } else {
+
+                document.getElementById(`idx${checkIfReveal[i]}`).classList.add("clicked")
+
+                for (el of checkGrid([checkIfReveal[i]],false)) {
+                    if (!checkIfReveal.includes(el)) {
+                        checkIfReveal.push(el)
+                    }
+                }
+
             }
         }
     }
-
-
-
-    
-    let arr = checkGrid(idx)
-    
 }
 
 function generateBombs() {
@@ -126,58 +152,51 @@ function generateBombs() {
 }
 
 function placeBombs() {
-
     for (el of bombsIdx) {
         grid[el] = "B"
     }
-  
 }
 
 function createGrid() {
+
     let arr = []
-    
+
     for (let i = 0; i < (board[0] * board[1]); i++) {
-        arr.push("")
+        arr.push(0)
     }
-    console.log(arr)
+
     return arr
 }
 
 function populateGrid() {
 
-    let arr = checkGrid(bombsIdx)
-    
+    let arr = checkGrid(bombsIdx,true)
+
     for (el of arr) {
-
-        if ((grid[el] === "") && grid[el] !== "B") {
-            grid[el] = 1
-
-        } else if (grid[el] !== "B") {
+        if (grid[el] !== "B") {
             grid[el] = grid[el] + 1
         }
     }
-
-    console.log(grid)
 }
 
-// function renderBoard() { //placeholder
+function renderBoard() { //placeholder
 
-//     for (i = 0; i < grid.length; i++) {
-//         if (grid[i]) {
-//             document.getElementById(`idx${i}`).innerText = grid[i]
-//         }
-//         if (grid[i] === "B") {
-//             document.getElementById(`idx${i}`).style.backgroundColor = "black";
-//             document.getElementById(`idx${i}`).style.color = "white";
-//         }
-//     }
-// }
+    for (i = 0; i < grid.length; i++) {
+        if (grid[i]) {
+            document.getElementById(`idx${i}`).innerText = grid[i]
+        }
+        if (grid[i] === "B") {
+            document.getElementById(`idx${i}`).style.backgroundColor = "black";
+            document.getElementById(`idx${i}`).style.color = "white";
+        }
+    }
+}
 
-function checkGrid(arrOfNums) {
+function checkGrid(arrOfIdx,boolean) {
 
     let arr = []
 
-    for (el of arrOfNums) {
+    for (el of arrOfIdx) {
 
         //row+1 col+0
         if (el + board[0] < board[0] * board[1]) { arr.push(el + board[0]) }
@@ -186,19 +205,24 @@ function checkGrid(arrOfNums) {
 
         //row+0 col+1
         if (el + 1 <= board[0] * board[1] && ((el + 1) % (board[0])) !== 0) { arr.push(el + 1) }
+        //row+0 col-1
+        if (el - 1 >= 0 && ((el % board[0]) !== 0)) { arr.push(el - 1) }
+
+
+        if (boolean){
         //row+1 col+1
         if (el + 1 + board[0] < board[0] * board[1] && ((el + 1) % board[0]) !== 0) { arr.push(el + board[0] + 1) }
         //row-1 col+1
         if (el + 1 - board[0] > 0 && ((el + 1) % board[0]) !== 0) { arr.push(el - board[0] + 1) }
 
-        //row+0 col-1
-        if (el - 1 >= 0 && ((el % board[0]) !== 0)) { arr.push(el - 1) }
+        
         //row+1 col-1
         if (el - 1 + board[0] < board[0] * board[1] && (el % board[0]) !== 0) { arr.push(el + board[0] - 1) }
         //row-1 col-1
         if (el - 1 - board[0] >= 0 && (el % board[0]) !== 0) { arr.push(el - board[0] - 1) }
+        }
     }
-    
+
     return arr
 }
 
