f9fc5da2c73129a4458e6dfb4457f9c9792bd42b
reddy <reddyfede@gmail.com>
2023-07-23 14:23:56 +0200

    add function to generate bomb position

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 /*----- app's state (variables) -----*/
 
-const begBoard = [9,9,10]
-const intBoard = [16,16,40]
-const expBoard = [30,16,100]
+const begBoard = [9, 9, 10]       //col,row,bombs
+const intBoard = [16, 16, 40]
+const expBoard = [30, 16, 99]
+const bombsIdx = []
+let board = []
 
 /*----- cached element references -----*/
 
 boardEl = document.querySelector(".board")
 
-selectLevelBtnEls = document.querySelectorAll(".selectLevel")
+selectLevelBtnEls = document.querySelectorAll(".levelBtn")
 
-resetBtn = document.querySelector("#reset")
+resetBtn = document.getElementById("reset")
+
+bombCounter = document.getElementById("bombCounter")
 
 /*----- event listeners -----*/
 
-for (btn of selectLevelBtnEls){
+for (btn of selectLevelBtnEls) {
     btn.addEventListener("click", defineBoard)
 }
 
@@ -24,14 +28,8 @@ resetBtn.addEventListener("click", init)
 
 /*----- functions -----*/
 
-init()
-
-function init() {
-    renderBoard()
-}
-
 function defineBoard(e) {
-    if (e.target.classList.contains("beg")){
+    if (e.target.classList.contains("beg")) {
         boardEl.classList.add("beginner")
         boardEl.classList.remove("intermediate")
         boardEl.classList.remove("expert")
@@ -39,17 +37,16 @@ function defineBoard(e) {
         boardEl.classList.remove("beginner")
         boardEl.classList.add("intermediate")
         boardEl.classList.remove("expert")
-    } else if (e.target.classList.contains("exp"))  {
+    } else if (e.target.classList.contains("exp")) {
         boardEl.classList.remove("beginner")
         boardEl.classList.remove("intermediate")
         boardEl.classList.add("expert")
     }
-    renderBoard()
+    createBoard()
 }
 
-function renderBoard() {
+function createBoard() {
 
-    let board
     deleteBoard()
 
     if (boardEl.classList.contains("beginner")) {
@@ -60,35 +57,57 @@ function renderBoard() {
         board = expBoard
     }
 
-    for (let i = 0; i < board[0]*board[1]; i++){
+    for (let i = 0; i < board[0] * board[1]; i++) {
         let newBtn = document.createElement("button")
         newBtn.classList.add("gameTile")
         newBtn.classList.add(`idx${i}`)
         boardEl.appendChild(newBtn)
     }
+
+    bombCounter.innerText = board[2]
+    generateBombs()
+    placeBombs()
+
 }
 
 function deleteBoard() {
     while (boardEl.firstChild) {
         boardEl.removeChild(boardEl.lastChild);
-      }    
+    }
 }
 
-function clickTile(e){
-    if (!e.target.classList.contains("gameTile")){
+function clickTile(e) {
+    if (!e.target.classList.contains("gameTile")) {
         return
     }
-    e.target.classList.add("clicked")
+    e.target.classList.add("clicked")  //placeholder
 }
 
+function generateBombs() {
 
+    bombsIdx.length = 0
 
+    while (bombsIdx.length < board[2]) {
+        let bomb = Math.floor(Math.random() * board[0] * board[1])
+        if (!bombsIdx.includes(bomb)) {
+            bombsIdx.push(bomb)
+        }
+    }
+}
 
+function placeBombs() {
+    for (let i = 0; i < bombsIdx.length - 1; i++) {
+        document.querySelector(`.idx${ bombsIdx[i]}`).innerText = "B"
+  }
+}
 
+function init() {
+    bombCounter.innerText = ""
+    bombsIdx.length = 0
 
+    createBoard()
+    console.log(bombsIdx)
+    console.log(bombsIdx.length)
+}
 
-
-
-
-
-
+init()
